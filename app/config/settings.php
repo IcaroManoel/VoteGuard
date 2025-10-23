b8db8cf31452c15e4dadb607dcf9edffaaa100a3
IcaroManoel <manoelicaro79@gmail.com>
2025-10-22 22:35:44 -0300

    bugs corrigidos

--- app/config/settings.php
+++ app/config/settings.php
@@ -1,3 +1,4 @@
+<?php 
 define('ROOT_PATH', __DIR__ . '/../');
 define('DATA_PATH', ROOT_PATH . 'data/');
 
@@ -6,3 +7,4 @@ define('POOLS_FILE', DATA_PATH . 'polls.json');
 define('CHECK_FRAUDE_BY_IP', true);
 define('CHECK_FRAUDE_BY_SESSION', true);
 define('MAX_VOTOS_ARQUIVO', 10000);
+?>
--- app/core/data_manager.php
+++ app/core/data_manager.php
@@ -1,5 +1,7 @@
+<?php 
 require_once __DIR__ . '/../config/settings.php';
 
-function reading_json(string, $file_path) {
+function reading_json(string $file_path) {
     
 }
+?>
